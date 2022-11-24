4b72d5e39e755de4dfccb53583eb1dc412de1960
Rodrigomagalhaesdev <112868249+Rodrigomagalhaesdev@users.noreply.github.com>
2022-11-24 12:20:37 -0300

    Add files via upload

---
+++ Acoes.js
@@ -0,0 +1,21 @@
+let resp = window.document.getElementById("saida");
+
+function acao1() {
+  resp.innerHTML += "<p>Clicou no primeiro botão</p>";
+  document.body.style.color = "#f2de00";
+}
+
+function acao2() {
+  resp.innerHTML += "<p>Clicou no segundo botão</p>";
+  document.body.style.color = "#1ca417";
+}
+
+function acao3() {
+  resp.innerHTML += "<p>Clicou no terceiro botão</p>";
+  document.body.style.color = "#1d00e0";
+}
+
+function acao4() {
+  resp.innerHTML += "<p>Clicou no quarto botão</p>";
+  document.body.style.color = "#f918b1";
+}
